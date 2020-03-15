d3c10970eec5bf1c1d13e4949d12da806977ead4
Petr Kostenko <picalines0@gmail.com>
2020-03-15 15:24:49 +0300

    rendering and scripts improvements

--- static/scripts/canvasPatternMousePainter.js
+++ static/scripts/canvasPatternMousePainter.js
@@ -1,32 +1,32 @@
-class CanvasGridPainter {
+class CanvasPatternMousePainter {
     /**
-     * @param {CanvasGrid} canvasGrid
+     * @param {CanvasPatternGrid} grid
      */
-    constructor(canvasGrid) {
-        this.canvasGrid = canvasGrid;
+    constructor(grid) {
+        this.grid = grid;
 
         this.cellValueToDraw = false;
 
         this.onstartdraw = null;
         this.onenddraw = null;
 
-        this.canvasGrid.canvas.onmousedown = (event) => {
+        this.grid.notesContext.canvas.onmousedown = (event) => {
             if (event.which != 1) return;
-            let { x, y } = this.getMouseGridPosition(event);
-            this.cellValueToDraw = !this.canvasGrid.getCell(x, y);
+            let [ x, y ] = this.getMouseGridPosition(event);
+            this.cellValueToDraw = !this.grid.getCell(x, y);
             if (this.onstartdraw) {
                 this.onstartdraw(x, y, this.cellValueToDraw);
             }
             
             this.drawCell(x, y);
 
-            this.canvasGrid.canvas.onmousemove = onmousemove;
+            this.grid.notesContext.canvas.onmousemove = onmousemove;
             window.addEventListener('mouseup', onmouseup);
         }
         
         const onmouseup = (event) => {
             if (event.which == 1) {
-                this.canvasGrid.canvas.onmousemove = null;
+                this.grid.notesContext.canvas.onmousemove = undefined;
                 if (this.onenddraw) {
                     this.onenddraw();
                 }
@@ -36,7 +36,7 @@ class CanvasGridPainter {
 
         const onmousemove = (event) => {
             if (event.which == 1) {
-                let { x, y } = this.getMouseGridPosition(event);
+                let [ x, y ] = this.getMouseGridPosition(event);
                 this.drawCell(x, y);
             }
         }
@@ -46,11 +46,11 @@ class CanvasGridPainter {
      * @param {MouseEvent} event 
      */
     getMouseGridPosition(event) {
-        const rect = this.canvasGrid.canvas.getBoundingClientRect();
-        return {
-            x: Math.trunc((event.clientX - rect.left) / this.canvasGrid.cellWidth),
-            y: Math.trunc((event.clientY - rect.top) / this.canvasGrid.cellHeight),
-        }
+        const rect = this.grid.notesContext.canvas.getBoundingClientRect();
+        return [
+            Math.trunc((event.clientX - rect.left) / this.grid.cellWidth),
+            Math.trunc((event.clientY - rect.top) / this.grid.cellHeight),
+        ]
     }
 
     /**
@@ -58,8 +58,6 @@ class CanvasGridPainter {
      * @param {number} y 
      */
     drawCell(x, y) {
-        if (this.canvasGrid.setCell(x, y, this.cellValueToDraw) && this.canvasGrid.cellsToRender.length > 0) {
-            this.canvasGrid.render();
-        }
+        this.grid.setCell(x, y, this.cellValueToDraw);
     }
 }
