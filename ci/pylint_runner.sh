f89db55eaf8f41ab94d45f8249ae80bb1a273afe
Evgenii Gladkov <jenya.love72@gmail.com>
2020-05-12 13:04:56 +0300

    CI ready

--- ci/pylint_runner.sh
+++ ci/pylint_runner.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-pylint manage.py main MusicShaper
-test
+pylint $(ls -d */) | tee pylint.txt
+mkdir public
+score=$(sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' pylint.txt)
+anybadge --value=$score --file=public/pylint.svg pylint
+echo "Pylint score was $score"
+pylint --load-plugins=pylint_json2html $(ls -d */) --output-format=jsonextended > pylint.json
+pylint-json2html -f jsonextended -o public/pylint.html pylint.json
+rm pylint.txt pylint.json
+exit 0
