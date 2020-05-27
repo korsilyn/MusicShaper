d47422a2ac72e1782b820cb0a4c1fd738d99ecb8
Petr Kostenko <picalines0@gmail.com>
2020-05-27 14:52:45 +0300

    instrument settings translate fix

---
+++ main/templatetags/list_tags.py
@@ -0,0 +1,22 @@
+'''
+Модуль вспомогательных фильтров django-шаблонов для строк
+'''
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter(name='index')
+def index_filter(arr, index):
+    '''
+    Возвращает элемент списка с индексом `index` (django-шаблоны)
+
+    Если указанного элемента нет, возвращает `None`
+    '''
+
+    try:
+        return arr[index]
+    except IndexError:
+        return None
